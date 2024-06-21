9be1b32885de61d3763ecf8ef36f2977ec579881
neazp <andreatrujilloazp@gmail.com>
2024-06-21 17:10:48 -0600

    ata Manejo de rutas

---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,18 @@
+import { Router, RouterOptions } from "express";
+
+class AuthRoutes {
+    public router: Router;
+
+    constructor(){
+        this.router = Router();
+        this.config();
+    }
+
+    config(){
+        this.router.get('/', (req, res)=> {
+            res.send('Incocando Autenticacion')
+        });
+    }
+}
+const authRoutes = new AuthRoutes();
+export default authRoutes.router;
